c9dc85ca106e3e97ab2dd017ba95c3d51248ca79
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-29 14:49:33 +0200

    Gjort funktion för sortering på kursnamn

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -11,7 +11,10 @@ import { CourseService } from '../services/course.service';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  // Array to save the fetched data in
   courseList: Course[] = [];
+  // A number property to use in sortName, to sort from a-z and z-a
+  sortingNum: number = 0;
   
   constructor(private courseservice: CourseService) { }
 
@@ -21,4 +24,18 @@ export class CoursesComponent {
     })
   }
 
+  sortName() {
+    // Add one for every click in the component
+    this.sortingNum = this.sortingNum + 1;
+    // If it's a odd number it will sort descending, else ascending
+    if(this.sortingNum % 2 != 0) {
+      this.courseList.sort((a, b) => a.coursename.localeCompare(b.coursename));
+    } else {
+      this.courseList.sort((a, b) => b.coursename.localeCompare(a.coursename));
+    }
+  }
+
+  sortCode() {
+
+  }
 }
