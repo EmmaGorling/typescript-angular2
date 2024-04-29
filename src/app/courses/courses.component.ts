537f3fc76f71d5b39e0b741988836c70e88740bb
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-29 17:11:42 +0200

    Alla funktioner för sortering klara

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,8 +13,10 @@ import { CourseService } from '../services/course.service';
 export class CoursesComponent {
   // Array to save the fetched data in
   courseList: Course[] = [];
-  // A number property to use in sortName, to sort from a-z and z-a
-  sortingNum: number = 0;
+  // Numbers to be able to toggle the sorting
+  sortNumName: number = 0;
+  sortNumCode: number = 0;
+  sortNumProg: number = 0;
   
   constructor(private courseservice: CourseService) { }
 
@@ -24,18 +26,34 @@ export class CoursesComponent {
     })
   }
 
-  sortName() {
+  sortName():void {
     // Add one for every click in the component
-    this.sortingNum = this.sortingNum + 1;
+    this.sortNumName = this.sortNumName + 1;
     // If it's a odd number it will sort descending, else ascending
-    if(this.sortingNum % 2 != 0) {
+    if(this.sortNumName % 2 != 0) {
       this.courseList.sort((a, b) => a.coursename.localeCompare(b.coursename));
     } else {
       this.courseList.sort((a, b) => b.coursename.localeCompare(a.coursename));
     }
   }
 
-  sortCode() {
+  sortCode():void {
+    // Add one for every click in the component
+    this.sortNumCode = this.sortNumCode + 1;
+    if(this.sortNumCode % 2 != 0) {
+      this.courseList.sort((a, b) => ( a.code > b.code) ? 1 : -1);
+    } else {
+      this.courseList.sort((a, b) => ( b.code > a.code) ? 1 : -1);
+    }
+  }
 
+  sortProg():void {
+    // Add one for every click in the component
+    this.sortNumProg = this.sortNumProg + 1;
+    if(this.sortNumProg % 2 != 0) {
+      this.courseList.sort((a, b) => ( a.progression > b.progression) ? 1 : -1);
+    } else {
+      this.courseList.sort((a, b) => ( b.progression > a.progression) ? 1 : -1);
+    }
   }
 }
