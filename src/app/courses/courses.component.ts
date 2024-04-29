db854d1ff3fc0313356e6a6357c12917d53c6b89
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-29 13:31:39 +0200

    Gjort interface och service för att hämta kurser Gjort tabell som skriver ut kurserna i komponenten

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  courseList: Course[] = [];
+  
+  constructor(private courseservice: CourseService) { }
+
+  ngOnInit() {
+    this.courseservice.getCourses().subscribe(data => {
+      this.courseList = data;
+    })
+  }
 
 }
