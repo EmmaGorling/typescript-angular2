d9ebc573cba694f6371ecb89d2e3b2d98181c607
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-29 19:28:46 +0200

    Gjort sökfunktion

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
@@ -17,12 +18,17 @@ export class CoursesComponent {
   sortNumName: number = 0;
   sortNumCode: number = 0;
   sortNumProg: number = 0;
-  
+  // Propery for searching
+  searchInput: string = '';
+  // Array for storing original data
+  originalCourseList: Course[] = [];
+
   constructor(private courseservice: CourseService) { }
 
   ngOnInit() {
     this.courseservice.getCourses().subscribe(data => {
       this.courseList = data;
+      this.originalCourseList = data;
     })
   }
 
@@ -30,7 +36,7 @@ export class CoursesComponent {
     // Add one for every click in the component
     this.sortNumName = this.sortNumName + 1;
     // If it's a odd number it will sort descending, else ascending
-    if(this.sortNumName % 2 != 0) {
+    if (this.sortNumName % 2 != 0) {
       this.courseList.sort((a, b) => a.coursename.localeCompare(b.coursename));
     } else {
       this.courseList.sort((a, b) => b.coursename.localeCompare(a.coursename));
@@ -40,7 +46,7 @@ export class CoursesComponent {
   sortCode():void {
     // Add one for every click in the component
     this.sortNumCode = this.sortNumCode + 1;
-    if(this.sortNumCode % 2 != 0) {
+    if (this.sortNumCode % 2 != 0) {
       this.courseList.sort((a, b) => ( a.code > b.code) ? 1 : -1);
     } else {
       this.courseList.sort((a, b) => ( b.code > a.code) ? 1 : -1);
@@ -50,10 +56,22 @@ export class CoursesComponent {
   sortProg():void {
     // Add one for every click in the component
     this.sortNumProg = this.sortNumProg + 1;
-    if(this.sortNumProg % 2 != 0) {
+    if (this.sortNumProg % 2 != 0) {
       this.courseList.sort((a, b) => ( a.progression > b.progression) ? 1 : -1);
     } else {
       this.courseList.sort((a, b) => ( b.progression > a.progression) ? 1 : -1);
+    }
+  }
+
+  searchTable():void {
+    // If search-input is not a empty string
+    if (this.searchInput.trim() !== '') {
+      // Check if coursename or code contains the searched phrase
+      this.courseList = this.originalCourseList.filter( course => 
+        course.coursename.toLowerCase().includes(this.searchInput.toLowerCase()) || course.code.toLowerCase().includes(this.searchInput.toLowerCase()))
+    } else {
+      // If the searchinput is empty, "reset" the array
+      this.courseList = this.originalCourseList;
     }
   }
 }
